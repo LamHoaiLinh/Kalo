2d0cb7489276fc42ccde97eb244a1ef79a2fe4b9
LamHoaiLinh <lamhoailinh105@gmail.com>
2026-09-24 18:03:16 +0700

    Add Kalo PWA service worker

---
+++ web/sw.js
@@ -0,0 +1,41 @@
+const CACHE='kalo-native-v1';
+const APP_SHELL=[
+  './',
+  './index.html',
+  './styles.css?v=1.0.0',
+  './app.js?v=1.0.0',
+  './config.js',
+  './crypto.js',
+  './webrtc.js',
+  './icon.svg',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install',(event)=>{
+  event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(APP_SHELL)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',(event)=>{
+  event.waitUntil(
+    caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE).map((key)=>caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',(event)=>{
+  const request=event.request;
+  if(request.method!=='GET')return;
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin)return;
+
+  event.respondWith(
+    fetch(request)
+      .then((response)=>{
+        const copy=response.clone();
+        caches.open(CACHE).then((cache)=>cache.put(request,copy)).catch(()=>{});
+        return response;
+      })
+      .catch(()=>caches.match(request).then((cached)=>cached||caches.match('./index.html')))
+  );
+});
