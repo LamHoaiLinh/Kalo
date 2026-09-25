91bfd2d54897754c8eca16da02593635c68c8fa0
LamHoaiLinh <lamhoailinh105@gmail.com>
2026-09-25 14:15:15 +0700

    Refresh Kalo cache for avatars and nicknames

--- web/sw.js
+++ web/sw.js
@@ -1,9 +1,9 @@
-const CACHE='kalo-native-v12-fixed-composer-privacy-shortcut';
+const CACHE='kalo-native-v13-avatars-nicknames';
 const APP_SHELL=[
   './',
   './index.html',
-  './styles.css?v=1.5.3',
-  './app.js?v=1.5.3',
+  './styles.css?v=1.6.0',
+  './app.js?v=1.6.0',
   './config.js',
   './crypto.js',
   './webrtc.js',
